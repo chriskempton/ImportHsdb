e3203032ce4171d851e98d44986fcc7dd587d3af
Chris Kempton <chris.kempton@ithaka.org>
2013-10-02 16:51:52 -0400

    I should really implement some tests at some point, I guess.

---
+++ src/test/java/org/kemptonfarms/substances/util/CassandraConnectionTest.java
@@ -0,0 +1,10 @@
+package org.kemptonfarms.substances.util;
+
+import org.junit.Test;
+
+public class CassandraConnectionTest {
+    @Test
+    public void testCreateConnection() throws Exception {
+
+    }
+}
